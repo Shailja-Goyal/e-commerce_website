1927a241234fad491f4d7c22e7afa624130bb5ed
Shailja-Goyal <53702309+Shailja-Goyal@users.noreply.github.com>
2020-12-16 00:39:28 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,10 @@
+<?php 
+session_start();
+require_once 'config/connect.php';
+include 'inc/header.php'; ?>
+<?php include 'inc/nav.php'; ?>
+<style>
+    html,body{
+        background-color:red;
+    }
+</style>
